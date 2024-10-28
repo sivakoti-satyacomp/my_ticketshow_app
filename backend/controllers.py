bd705e0a34856425017efe231651f7af458aff16
sivakoti-satyacomp <satyanarayana@study.iitm.ac.in>
2024-10-28 18:12:41 +0530

    Commit on 28th Oct 2024

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,8 +1,8 @@
 #App routes
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
 from .models import *
 from flask import current_app as app
-
+from datetime import datetime
 
 @app.route("/")
 def home():
@@ -16,9 +16,9 @@ def signin():
         pwd=request.form.get("password")
         usr=User_Info.query.filter_by(email=uname,password=pwd).first()
         if usr and usr.role==0: #Existed and admin
-            return render_template("admin_dashboard.html")
+            return redirect(url_for("admin_dashboard",name=uname))
         elif usr and usr.role==1: #Existed and normal user
-            return render_template("user_dashboard.html")
+            return redirect(url_for("user_dashboard",name=uname))
         else:
             return render_template("login.html",msg="Invalid user credentials...")
 
@@ -43,5 +43,51 @@ def signup():
     
     return render_template("signup.html",msg="")
 
-#Many controllers/routers here
+#Common route for admin dashboard
+@app.route("/admin/<name>")
+def admin_dashboard(name):
+    theatres=get_theatres()
+    return render_template("admin_dashboard.html",name=name,theatres=theatres)
 
+
+@app.route("/user/<name>")
+def user_dashboard(name):
+    return render_template("user_dashboard.html",name=name)
+
+#Many controllers/routers here
+@app.route("/venue/<name>",methods=["POST","GET"])
+def add_venue(name):
+    if request.method=="POST":
+        vname=request.form.get("name")
+        location=request.form.get("location")
+        pin_code=request.form.get("pin_code")
+        capacity=request.form.get("capacity")
+        new_theatre=Theatre(name=vname,location=location,pin_code=pin_code,capacity=capacity)
+        db.session.add(new_theatre)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    
+    return render_template("add_venue.html",name=name)
+
+@app.route("/show/<venue_id>/<name>",methods=["POST","GET"])
+def add_show(venue_id,name):
+    if request.method=="POST":
+        sname=request.form.get("name")
+        tags=request.form.get("tags")
+        tkt_price=request.form.get("tkt_price")
+        date_time=request.form.get("dt_time") #data is string format
+        #print(date_time)
+        #processing date/time
+        dt_time=datetime.strptime(date_time,"%Y-%m-%dT%H:%M")
+        new_show=Show(name=sname,tags=tags,tkt_price=tkt_price,date_time=dt_time,theatre_id=venue_id)
+        db.session.add(new_show)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    
+    return render_template("add_show.html",venue_id=venue_id,name=name)
+        
+
+#Other supported function
+def get_theatres():
+    theatres=Theatre.query.all()
+    return theatres
